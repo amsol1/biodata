98688d3c7474fe447e5f52016a7cedbab6aed439
amsol79 <30430136+amsol79@users.noreply.github.com>
2019-05-20 02:35:17 +0700

    Update tese.php

--- tese.php
+++ tese.php
@@ -3,7 +3,7 @@
    // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:dicodingwebappamsolserver.database.windows.net,1433; 
-    Database = dicodingamsoldb", "dicoding", "{rakan@ra19}");
+    Database = dicodingamsoldb", "dicoding", "rakan@ra19");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
