43692abb57111a2b124aea5455b547b9a89d0846
amsol79 <30430136+amsol79@users.noreply.github.com>
2019-05-20 03:44:52 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,16 +3,16 @@
 
 // SQL Server Extension Sample Code:
 
-    $host = "tcp:dicodingwebappamsolserver.database.windows.net";
-    $user = "dicoding";
-    $pass = "rakan@ra19";
-    $db = "dicodingamsoldb";
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:dicodingwebappamsolserver.database.windows.net,1433; Database = dicodingamsoldb", "dicoding", "{rakan@ra19}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
 
     ?>
 
