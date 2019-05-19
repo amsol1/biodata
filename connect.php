f8b21324d89231cb75aeb9e0e40b5889cf6603f0
amsol79 <30430136+amsol79@users.noreply.github.com>
2019-05-20 03:39:32 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 <?php  
 
 // SQL Server Extension Sample Code:
-<?php
+
     
 $connectionInfo = array("UID" => "dicoding", "pwd" => "{rakan@ra19}", "Database" => "dicodingamsoldb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:dicodingwebappamsolserver.database.windows.net,1433";
