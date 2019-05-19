7782621bc49f6369cf275ae1662c5647fa5bfe85
amsol79 <30430136+amsol79@users.noreply.github.com>
2019-05-20 02:43:24 +0700

    Update tese.php

--- tese.php
+++ tese.php
@@ -1,6 +1,6 @@
 <?php  
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "dicoding@dicodingwebappamsolserver", "pwd" => "{rakan@ra199}", "Database" => "dicodingamsoldb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "dicoding@dicodingwebappamsolserver", "pwd" => "{rakan@ra19}", "Database" => "dicodingamsoldb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:dicodingwebappamsolserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
     $tsql= "select * from biodata";
